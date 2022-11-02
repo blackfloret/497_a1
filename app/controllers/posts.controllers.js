6929b717c02c7e51595c5306ec1fb79bc12f2e1f
blackfloret <blackfloret@gmail.com>
2022-11-01 23:42:07 -0700

    Caching Posts

--- app/controllers/posts.controllers.js
+++ app/controllers/posts.controllers.js
@@ -1,5 +1,7 @@
 const { Post } = require("../models/post");
-
+const Redis = require("ioredis");
+const Cache = require("ioredis-cache");
+const cache = new Cache(new Redis("blogcluster.ntwtkd.ng.0001.usw2.cache.amazonaws.com"));
 
 const homeStartingContent =
 	'The home pages lists all the blogs from all the users.';
@@ -29,7 +31,9 @@ const displayAllPosts = async (req, res) => {
 };
 async function displayPost (req, res)  {
 	const requestedPostId = req.params.postId;
-	const post = await Post.findOne({where: { id: requestedPostId}});
+	//const post = await Post.findOne({where: { id: requestedPostId}});
+	console.log(cache.getCache(requestedPostId));
+	const post = await cache.cache(requestedPostId, async () => await Post.findOne({where: { id: requestedPostId}}))
 	res.render('post', {
 		title: post.tile,
 		content: post.content
